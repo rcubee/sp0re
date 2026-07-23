a28d08f088ceb1843f1b26ea8752eed2cbdcd2b9
rcubee <kakapokakapo@protonmail.com>
2026-07-23 19:53:09 +0200

    add SP0RT_CONF_ASSERT macro to enable the use of assertions.

--- include/sp0re_conf_internal.h
+++ include/sp0re_conf_internal.h
@@ -4,16 +4,16 @@
 #include "sp0re_conf.h"
 
 #ifndef SP0RE_CONF_CORE_CLK_HZ
-    #error "SP0RE_CONF_CORE_CLK_HZ must be defined"
-#endif
+    #error "SP0RE_CONF_CORE_CLK_HZ must be defined."
+#endif // SP0RE_CONF_CORE_CLK_HZ
 
 #ifndef SP0RE_CONF_TICK_RATE_HZ
-    #error "SP0RE_CONF_TICK_RATE_HZ must be defined"
-#endif
+    #error "SP0RE_CONF_TICK_RATE_HZ must be defined."
+#endif // SP0RE_CONF_TICK_RATE_HZ
 
 #ifndef SP0RE_CONF_MAX_THREAD_COUNT
-    #error "SP0RE_CONF_MAX_THREAD_COUNT must be defined"
-#endif
+    #error "SP0RE_CONF_MAX_THREAD_COUNT must be defined."
+#endif // SP0RE_CONF_MAX_THREAD_COUNT
 
 #ifdef SP0RE_CONF_IDLE_THREAD_FUNC
     #ifndef SP0RE_CONF_IDLE_THREAD_STACK_CAPACITY
@@ -23,7 +23,13 @@
     #define SP0RE_CONF_IDLE_THREAD_FUNC sp0re_default_idle_thread_func
     #define SP0RE_CONF_IDLE_THREAD_STACK_CAPACITY (128U)
     #define SP0RE_USING_DEFAULT_IDLE_THREAD_FUNC
-#endif
+#endif // SP0RE_CONF_IDLE_THREAD_FUNC
+
+#ifdef SP0RE_CONF_ASSERT
+    #define SP0RE_ASSERT(expression) SP0RE_CONF_ASSERT(expression)
+#else // SP0RE_CONF_ASSERT
+    #define SP0RE_ASSERT(expression) (void)0
+#endif // SP0RE_CONF_ASSERT
 
 #define SYSTICK_RVR_RELOAD_VALUE ((SP0RE_CONF_CORE_CLK_HZ / SP0RE_CONF_TICK_RATE_HZ) - 1U)
 _Static_assert((SYSTICK_RVR_RELOAD_VALUE >= 0x00000001U && SYSTICK_RVR_RELOAD_VALUE <= 0x00FFFFFFU), "SysTick RVR RELOAD value is out of range.");
